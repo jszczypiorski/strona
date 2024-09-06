dd72c9b98c97e85a698414af5850665f16fd2a60
Jakub Szczypiorski <jakub.szczypiorskii@gmail.com>
2024-09-06 02:23:56 +0200

    yes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 const Header = () => (
-  <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">
-          Wyszukiwarka przepisów - Znajdź jakiś fajny przepis!
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </Box>
+  <AppBar position="static">
+    <Toolbar>
+      <Typography Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Wyszukiwarka przepisów - Znajdź jakiś fajny przepis!
+      </Typography>
+    </Toolbar>
+  </AppBar>
 );
 
 export default Header;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,56 +1,57 @@
-/* eslint-disable react/jsx-key */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import {
-  Grid,
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  Box,
-  styled,
-} from "@mui/material";
-import React from "react";
+import styled from "styled-components";
+import { Card, Typography, Grid, CardContent } from "@mui/material";
 
-const StyledCard = styled(Card)`
+const RecipeCard = styled(Card)`
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const RecipeImage = styled.img`
+  height: 200px;
+  object-fit: cover;
+`;
+
+const RecipeTitle = styled(Typography)`
+  font-weight: bold;
+  margin-bottom: 8px;
+`;
+
+const RecipeIngredients = styled.div`
+  margin-bottom: 16px;
 `;
 
 export const RecipesList = ({ recipes }) => {
   return (
     <Grid container spacing={3}>
-      {recipes.map((recipe, index) => (
-        <Grid item xs={12} sm={6} md={4} key={index}>
-          <Card sx={{ height: "100%" }}>
-            <CardMedia
-              alt={recipe.recipe.label}
-              image={recipe.recipe.image}
-              component="img"
-              sx={{ height: "220px" }}
-            />
-            <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-              <Typography gutterBottom variant="h5" component="div">
-                {recipe.recipe.label}
-              </Typography>
-              <Typography variant="body2" color="textSecondary">
-                Calories: {Math.round(recipe.recipe.calories)} kcal
-              </Typography>
-              <Box sx={{ flex: 1 }}>
-                {recipe.recipe.ingredientLines.map((ingredientLine, idx) => (
-                  <Typography
-                    gutterBottom
-                    variant="body2"
-                    component="div"
-                    key={idx}
-                  >
-                    {ingredientLine}
-                  </Typography>
-                ))}
-              </Box>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
+      {recipes.map((recipe, index) => {
+        const { label, image, calories, totalWeight } = recipe.recipe;
+        const caloriesPer100g = totalWeight
+          ? (calories / totalWeight) * 100
+          : 0;
+
+        return (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+            <RecipeCard>
+              <RecipeImage src={image} alt={label} />
+              <CardContent sx={{ flex: 1 }}>
+                <RecipeTitle variant="h6">{label}</RecipeTitle>
+                <RecipeIngredients>
+                  {recipe.recipe.ingredientLines.map((ingredientLine, i) => (
+                    <Typography key={i} variant="body2" color="textSecondary">
+                      {ingredientLine}
+                    </Typography>
+                  ))}
+                </RecipeIngredients>
+                <Typography variant="body2" color="textSecondary">
+                  Calories per 100g: {Math.round(caloriesPer100g)} kcal
+                </Typography>
+              </CardContent>
+            </RecipeCard>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 };
-export default RecipesList;
